5599b775df91a38598a210248ed79e8c76281edf
numao <taxnumao@gmail.com>
2019-10-23 08:56:47 +0900

    delete other files

--- js/shopping.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var entry_url = $("#entry_url").val();
-
-    $("#cart_in").click(function(){
-        var item_id = $("#item_id").val();
-        location.href = entry_url + "cart.php?item_id=" + item_id;
-    });
-});
